3edfe09aec876f8caa1a5ca2babfceb043ed04ff
Sasial <44125644+sasial-dev@users.noreply.github.com>
2021-07-03 17:19:07 +1000

    Add order structure & constructor

--- src/ReplicatedStorage/order.ts
+++ src/ReplicatedStorage/order.ts
@@ -1,6 +1,47 @@
+import { t } from "@rbxts/t";
+import { Players } from "@rbxts/services";
+import DataStore2 from "@rbxts/datastore2";
+import config from "Workspace/config";
+
 class Order {
-	constructor(creator: Player, receiver: string, items: string[]) {
-		print("hi");
+	creator: Player;
+	receiver: Player;
+	items: string[];
+	orderClaimer?: Player;
+	id: number;
+	isClaimed: boolean;
+	isCompleted: boolean;
+
+	constructor(creator: Player, receiver: string | Player, items: string[], id: number) {
+		// Init. values
+		this.creator = creator;
+		this.items = items;
+		if (t.userdata(receiver)) {
+			this.receiver = receiver;
+		} else {
+			let receiverPlayer: Player | undefined;
+			for (const plr of Players.GetPlayers()) {
+				if (plr.Name === receiver) {
+					receiverPlayer = plr;
+				}
+			}
+			if (receiverPlayer) {
+				this.receiver = receiverPlayer;
+			} else {
+				error(`[LundstrongOrders] Unable to find player ${receiver}`);
+			}
+		}
+		this.isClaimed = false;
+		this.isCompleted = false;
+		this.id = id;
+
+		// Points
+		if (creator.Name !== receiver) {
+			if (config.PointsSettings.PointsEnabled) {
+				const dataStore = DataStore2("points", creator);
+				dataStore.Increment(config.PointsSettings.PointsAwarded);
+			}
+		}
 	}
 }
 
